64951f5a3414171129aa29e0b6792aa72dc19d70
Mariusz Batyra <mariusz.batyra@allegro.pl>
2025-11-15 00:15:49 +0100

    data initializer

--- src/main/kotlin/com/e1s/hackathon/controller/dto/NotificationChannelDto.kt
+++ src/main/kotlin/com/e1s/hackathon/controller/dto/NotificationChannelDto.kt
@@ -4,6 +4,6 @@ package com.e1s.hackathon.controller.dto
  * DTO for NotificationChannel data transfer.
  */
 enum class NotificationChannelDto {
-    EMAIL, SMS, WHATSAPP
+    EMAIL, SMS, WHATSAPP, MS_TEAMS
 }
 
